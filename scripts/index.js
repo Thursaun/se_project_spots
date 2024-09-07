d6186334f165942ac67b37743324633ba13b37d5
Thursaun <Thursaun.Canton@gmail.com>
2024-09-07 19:09:44 -0400

    Project 4 Stage 4 - Cards JS

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,27 @@ const initialCards = [
   },
 ];
 
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list")
 
+function getCardElement(data) {
+  console.log(data)
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image")
+
+  cardNameEl.textContent = data.name;
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+
+  return cardElement;
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
 
 
 
